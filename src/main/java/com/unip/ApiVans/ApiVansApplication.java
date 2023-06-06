b180369a9f5a75c0c7a2ed174137709dedc05fc1
Lucas Pomar <lucaspomar@outlook.com>
2023-06-06 20:57:08 -0300

    Criando projeto

---
+++ src/main/java/com/unip/ApiVans/ApiVansApplication.java
@@ -0,0 +1,13 @@
+package com.unip.ApiVans;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiVansApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiVansApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/unip/ApiVans/ApiVansApplicationTests.java
@@ -0,0 +1,13 @@
+package com.unip.ApiVans;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiVansApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
