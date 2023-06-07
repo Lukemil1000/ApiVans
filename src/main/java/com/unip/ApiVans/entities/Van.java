1a1d4fcd954d51720df2bde5970c78acfaf9cfb1
Lucas Pomar <lucaspomar@outlook.com>
2023-06-07 19:55:22 -0300

    Configurando banco h2

--- src/main/java/com/unip/ApiVans/entities/Van.java
+++ src/main/java/com/unip/ApiVans/entities/Van.java
@@ -1,16 +1,23 @@
 package com.unip.ApiVans.entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.io.Serializable;
 
+@Entity
 @NoArgsConstructor
 @AllArgsConstructor
 @Data
 public class Van implements Serializable {
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
     private String placa;
     private String modelo;
