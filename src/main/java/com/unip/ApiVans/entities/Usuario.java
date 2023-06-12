7d3894651ef9f8233818a5b9d8d227096b88fd08
Lucas Pomar <lucaspomar@outlook.com>
2023-06-12 18:26:00 -0300

    Entidade Usuario

--- src/main/java/com/unip/ApiVans/entities/Usuario.java
+++ src/main/java/com/unip/ApiVans/entities/Usuario.java
@@ -8,7 +8,6 @@ import lombok.Setter;
 import org.hibernate.Hibernate;
 
 import java.io.Serializable;
-import java.sql.Timestamp;
 import java.util.Objects;
 
 @AllArgsConstructor
@@ -16,35 +15,38 @@ import java.util.Objects;
 @Getter
 @Setter
 @Entity
-@Table(name = "registro_falha")
-public class RegistroFalha implements Serializable {
+@Table(name = "usuario")
+public class Usuario implements Serializable {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id", nullable = false)
     private Long id;
 
-    @Column(name = "placa")
-    private String placa;
+    @Column(name = "nome")
+    private String nome;
 
-    @Column(name = "horario")
-    private Timestamp horario;
+    @Column(name = "login")
+    private String login;
+
+    @Column(name = "senha")
+    private String senha;
+
+    @Column(name = "nivel_acesso")
+    private Integer nivelAcesso;
 
     @Column(name = "id_escola")
     private Long idEscola;
 
-    @Column(name = "qrcode")
-    private String qrcode;
-
-    @Column(name = "motivo")
-    private String motivo;
+    @Column(name = "telefone")
+    private Integer telefone;
 
     @Override
     public boolean equals(Object o) {
         if (this == o) return true;
         if (o == null || Hibernate.getClass(this) != Hibernate.getClass(o)) return false;
-        RegistroFalha that = (RegistroFalha) o;
-        return id != null && Objects.equals(id, that.id);
+        Usuario usuario = (Usuario) o;
+        return id != null && Objects.equals(id, usuario.id);
     }
 
     @Override
